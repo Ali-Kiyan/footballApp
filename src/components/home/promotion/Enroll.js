fe4f62c826906e0de18a42bde46bbb1a31df57e6
Ali <alikiyand@gmail.com>
2019-08-30 20:21:15 +0100

    dynamic form field-halfway

--- src/components/home/promotion/Enroll.js
+++ src/components/home/promotion/Enroll.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Fade } from 'react-reveal';
+import FormField from '../../misc/formFields';
 
-const PromotionAnimation = () => {
-    return (
-        <div>
-            animation
-        </div>
-    );
-};
+export class Enroll extends Component {
 
-export default PromotionAnimation;
+    state = {
+        formError: false,
+        formSuccess: "",
+        formdata: {
+            email:  {
+                element: 'input',
+                value: '',
+                config: {
+                    name: 'email_input',
+                    type: 'email',
+                    placeholder: 'Enter your email'
+                },
+                validation: {
+                    required: true,
+                    email: true
+                },
+                valid: false,
+                validationMessage: ''
+            }
+        }
+    }
+
+    submitForm(){
+
+    }
+
+
+    render() {
+        return (
+            <Fade>
+              <div className="enroll_wrapper">
+                  <form onSubmit={(event)=>this.submitForm(event)}>
+                    <div className="enroll_title">
+                        Enter your email
+                    </div>
+                    <div className="enroll_input">
+                        <FormField 
+                            id={'email'}
+                            formdata={this.state.formdata.email}
+                        />
+                    </div>
+                  </form>
+              </div>  
+            </Fade>
+        );
+    }
+}
+
+export default Enroll;
