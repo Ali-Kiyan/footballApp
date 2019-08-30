8bf901e007a4409d7d7c3e7d3ac94f730a60854a
Ali <alikiyand@gmail.com>
2019-08-30 18:44:17 +0100

    added meet the team

--- src/components/home/meetPlayers/index.js
+++ src/components/home/meetPlayers/index.js
@@ -29,6 +29,36 @@ export class MeetPlayers extends Component {
                                     Meet
                                 </Tag>
                             </div>
+                            <div>
+                                <Tag bck="#0e1731" size="100px" color="#fff"
+                                add={{
+                                    display: 'inline-block',
+                                    marginBottom: '30px'
+                                }}
+                                >
+                                    The
+                                </Tag>
+                            </div>
+                            <div>
+                                <Tag bck="#0e1731" size="100px" color="#fff"
+                                add={{
+                                    display: 'inline-block',
+                                    marginBottom: '20px'
+                                }}
+                                >
+                                    Players
+                                </Tag>
+                            </div>
+                            <div>
+                                <Tag bck="#fff" size="27px" color="#0e1731" link linkTo="the_team"
+                                 add={{
+                                    display: 'inline-block',
+                                    marginBottom: '27px',
+                                    border: '1px solid #0e1731'
+                                }}>
+                                    Meet them here
+                                </Tag>
+                            </div>
 
                         </div>
                     </div>
