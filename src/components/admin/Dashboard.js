466b3e2d01e6e93e2c55902f57c471147dc1e562
Ali <alikiyand@gmail.com>
2019-09-16 23:25:19 +0100

    added dashboard scaffoldings

---
+++ src/components/admin/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div className="user_dashboard">
+            <div>
+                this is the dashboard
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import Layout from './HoC/Layout';
 import { Switch, Route } from 'react-router-dom'
 import Home from './components/home';
 import SignIn from './components/signin'
+import Dashboard from './components/admin/Dashboard'
 const Routes = (props) => {
   return (
     <div>
         <Layout>
           <Switch>
+            <Route exact component={SignIn} path="/dashboard"></Route>
             <Route exact component={SignIn} path="/sign_in"></Route>
             <Route exact component={Home} path="/"></Route>
           </Switch>
