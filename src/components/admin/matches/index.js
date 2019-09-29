a5782e3e9b41d2369a604a7ca79caac09f3f0c42
Ali <alikiyand@gmail.com>
2019-09-29 15:09:01 +0100

    matchlist

--- src/components/admin/matches/index.js
+++ src/components/admin/matches/index.js
@@ -1,13 +1,96 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import AdminLayout from '../../../HoC/AdminLayout';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import { firebaseMatches, firebase } from '../../../firebase';
+import { firebaseLooper } from '../../misc/firebaseLooper';
+import { reverseArray } from '../../misc/utility';
 
 
 export class AdminMatches extends Component {
+
+    state = {
+        isLoading: true,
+        matches: []
+    }
+
+    componentDidMount(){
+        firebaseMatches.once('value').then( snapshot => {
+            const matches = firebaseLooper(snapshot);
+            this.setState({
+                isLoading: false,
+                matches: reverseArray(matches)
+            })
+        })
+    }
+
     render() {
+        console.log(this.state)
         return (
             <AdminLayout>
-                Table
+                <div>
+                    <Paper>
+                        <Table>
+                            <TableHead>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Date</TableCell>
+                                        <TableCell>Match</TableCell>
+                                        <TableCell>Result</TableCell>
+                                        <TableCell>Final</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    { this.state.matches ? 
+                                            this.state.matches.map((match, index) => (
+                                                <TableRow key={index}>
+                                                    <TableCell>
+                                                        {match.date}
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        <Link to={`/admin_matches/edit_matches/${match.id}`}>
+                                                            {match.away} <strong>-</strong> {match.local}
+                                                        </Link>
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        {match.resultAway} <strong>-</strong> {match.resultLocal}
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        {
+                                                            match.final === 'Yes' ? 
+                                                            <span className="matches_tag_red">Final</span>
+                                                            :
+                                                            <span className="matches_tag_green">Not played yet</span>
+                                                        }
+                                                    </TableCell>
+
+                                                </TableRow>
+                                            ))
+                                        : null
+                                    }
+                                    <TableRow>
+
+                                    </TableRow>
+
+                                </TableBody>
+
+                            </TableHead>
+                        </Table>
+                        <div className="admin_progress">
+                            {this.state.isLoading ? <CircularProgress thickness={7} style={{
+                                color: '#98c5e9'
+                            }}/> : '' }
+                        </div>
+                    </Paper>
+                </div>
             </AdminLayout>
         );
     }
